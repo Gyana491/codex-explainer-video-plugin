ba682882dd127411362b64262108406ec0ebfe66
Gyana Ranjan <gyanaranjanmohanta491@gmail.com>
2026-07-16 02:43:49 +0530

    remove codex-explainer-video-plugin directory and associated files

--- codex-explainer-video-plugin/worker-configuration.d.ts
+++
@@ -1,8 +0,0 @@
-interface Env {
-  MEDIA_BUCKET: R2Bucket;
-  OPENAI_API_KEY: string;
-  REPLICATE_API_TOKEN: string;
-  PUBLIC_MEDIA_BASE_URL: string;
-  REPLICATE_MODEL: string;
-  OPENAI_TTS_MODEL: string;
-}
